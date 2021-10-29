4ce640fbe91c5ef1e7e2152fbf43fbfbe4fc2835
birego <93344052+birego@users.noreply.github.com>
2021-10-29 20:45:45 +0200

    Add files via upload

---
+++ JackLeventer.py
@@ -0,0 +1,13 @@
+#intruision instagram
+#explosion du stockage
+#virus mutant
+import os
+import sys
+
+def banner():
+    print( """ \n
+###### ##### # ##### # # ## ##### ##### # # # # # # # # # # # # # # # # # # # ###### # # # # # # ##### # # ##### # # ###### # ##### # # ####### # # # # # # # # # # # # # # # # # # # ####### # #####
+    """)
+    print("243\n***********JackLeventer************")
+banner()
+
